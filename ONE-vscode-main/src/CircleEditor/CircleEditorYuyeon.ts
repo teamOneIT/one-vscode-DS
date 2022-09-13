7cc6bf7eaa62c03f0411c7344dee2aad27c249c4
YuyeonHan <hyy0416@naver.com>
2022-09-13 15:40:31 +0900

    editor Document, Provider 구현중

--- ONE-vscode-main/src/CircleEditor/CircleEditorYuyeon.ts
+++ ONE-vscode-main/src/CircleEditor/CircleEditorYuyeon.ts
@@ -15,15 +15,13 @@
  */
 
 import * as vscode from 'vscode';
-
+import { Disposable, disposeAll } from './dispose';
 import {CircleGraphCtrl} from './CircleEditorCtrl';
+import * as Circle from './circle_schema_generated';
 
 
-/**
- * @brief Viewer control with CircleGraphCtrl
- */
-/* istanbul ignore next */
-class CircleViewer extends CircleGraphCtrl {
+
+class CircleEditor extends CircleGraphCtrl {
   private readonly _panel: vscode.WebviewPanel;
 
   constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
@@ -40,51 +38,192 @@ class CircleViewer extends CircleGraphCtrl {
   }
 }
 
-/**
- * @brief Readonly CustomDocument for circle model
- * @note  Actual content is handled by CircleGraphCtrl itself so documment
- *        only provides URI of the file
- */
-/* istanbul ignore next */
-export class CircleViewerDocument implements vscode.CustomDocument {
-  private readonly _uri: vscode.Uri;
-  private _circleViewer: CircleViewer[];
+interface CircleEditorDocumentDelegate { //?
+	getFileData(): Promise<Uint8Array>;
+}
 
-  static async create(uri: vscode.Uri):
-      Promise<CircleViewerDocument|PromiseLike<CircleViewerDocument>> {
-    return new CircleViewerDocument(uri);
-  }
 
-  private constructor(uri: vscode.Uri) {
-    this._uri = uri;
-    this._circleViewer = [];
-  }
+interface CircleEdits{
+  content: string; //edit할 사항 작성
+}
 
-  public get uri() {
-    return this._uri;
-  }
+class OperatorEdits implements CircleEdits{
+  content = "operator";
+ //edit 내용
+}
 
-  // CustomDocument implements
-  dispose(): void {
-    // NOTE panel is closed before document and this is just for safety
-    this._circleViewer.forEach((view) => {
-      view.disposeGraphCtrl();
-    });
-    this._circleViewer = [];
-  }
+export class CircleEditorDocument extends Disposable implements vscode.CustomDocument {
+  
+  static async create(
+		uri: vscode.Uri,
+		backupId: string | undefined,
+		delegate: CircleEditorDocumentDelegate,
+	): Promise<CircleEditorDocument | PromiseLike<CircleEditorDocument>> {
+		// If we have a backup, read that. Otherwise read the resource from the workspace
+		const dataFile = typeof backupId === 'string' ? vscode.Uri.parse(backupId) : uri;
+		const fileData = await CircleEditorDocument.readFile(dataFile);
+		return new CircleEditorDocument(uri, fileData, delegate);
+	}
 
+	private static async readFile(uri: vscode.Uri): Promise<Uint8Array> {
+		if (uri.scheme === 'untitled') {
+			return new Uint8Array();
+		}
+		return new Uint8Array(await vscode.workspace.fs.readFile(uri));
+	}
+
+	private readonly _uri: vscode.Uri;
+
+	private _documentData: Uint8Array;
+	private _edits: Array<CircleEdits> = [];
+	private _savedEdits: Array<CircleEdits> = [];
+  private _circleEditor: CircleEditor[];
+
+	private readonly _delegate: CircleEditorDocumentDelegate;
+
+	private constructor(
+		uri: vscode.Uri,
+		initialContent: Uint8Array,
+		delegate: CircleEditorDocumentDelegate
+	) {
+		super();
+		this._uri = uri;
+		this._documentData = initialContent;
+		this._delegate = delegate;
+    this._circleEditor = [];
+	}
+
+	public get uri() { return this._uri; }
+
+	public get documentData(): Uint8Array { return this._documentData; }
+
+	private readonly _onDidDispose = this._register(new vscode.EventEmitter<void>());
+	/**
+	 * Fired when the document is disposed of.
+	 */
+	public readonly onDidDispose = this._onDidDispose.event;
+
+	private readonly _onDidChangeDocument = this._register(new vscode.EventEmitter<{
+		readonly content?: Uint8Array;
+		readonly edits: readonly CircleEdits[];
+	}>());
+	/**
+	 * Fired to notify webviews that the document has changed.
+	 */
+	public readonly onDidChangeContent = this._onDidChangeDocument.event;
+
+	private readonly _onDidChange = this._register(new vscode.EventEmitter<{
+		readonly label: string,
+		undo(): void,
+		redo(): void,
+	}>());
+	/**
+	 * Fired to tell VS Code that an edit has occurred in the document.
+	 *
+	 * This updates the document's dirty indicator.
+	 */
+	public readonly onDidChange = this._onDidChange.event;
+
+	/**
+	 * Called by VS Code when there are no more references to the document.
+	 *
+	 * This happens when all editors for it have been closed.
+	 */
+	dispose(): void {
+		this._onDidDispose.fire();
+		super.dispose();
+	}
+
+	/**
+	 * Called when the user edits the document in a webview.
+	 *
+	 * This fires an event to notify VS Code that the document has been edited.
+	 */
+	makeEdit(edit: CircleEdits) {
+		this._edits.push(edit);
+
+		this._onDidChange.fire({
+			label: 'Stroke',
+			undo: async () => {
+				this._edits.pop();
+				this._onDidChangeDocument.fire({
+					edits: this._edits,
+				});
+			},
+			redo: async () => {
+				this._edits.push(edit);
+				this._onDidChangeDocument.fire({
+					edits: this._edits,
+				});
+			}
+		});
+	}
+
+	/**
+	 * Called by VS Code when the user saves the document.
+	 */
+	async save(cancellation: vscode.CancellationToken): Promise<void> {
+		await this.saveAs(this.uri, cancellation);
+		this._savedEdits = Array.from(this._edits);
+	}
+
+	/**
+	 * Called by VS Code when the user saves the document to a new location.
+	 */
+	async saveAs(targetResource: vscode.Uri, cancellation: vscode.CancellationToken): Promise<void> {
+		const fileData = await this._delegate.getFileData();
+		if (cancellation.isCancellationRequested) {
+			return;
+		}
+		await vscode.workspace.fs.writeFile(targetResource, fileData);
+	}
+
+	/**
+	 * Called by VS Code when the user calls `revert` on a document.
+	 */
+	async revert(_cancellation: vscode.CancellationToken): Promise<void> {
+		const diskContent = await CircleEditorDocument.readFile(this.uri);
+		this._documentData = diskContent;
+		this._edits = this._savedEdits;
+		this._onDidChangeDocument.fire({
+			content: diskContent,
+			edits: this._edits,
+		});
+	}
+
+	/**
+	 * Called by VS Code to backup the edited document.
+	 *
+	 * These backups are used to implement hot exit.
+	 */
+	async backup(destination: vscode.Uri, cancellation: vscode.CancellationToken): Promise<vscode.CustomDocumentBackup> {
+		await this.saveAs(destination, cancellation);
+
+		return {
+			id: destination.toString(),
+			delete: async () => {
+				try {
+					await vscode.workspace.fs.delete(destination);
+				} catch {
+					// noop
+				}
+			}
+		};
+	}
+
+//add openView
   public openView(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-    let view = new CircleViewer(panel, extensionUri);
+    let view = new CircleEditor(panel, extensionUri);
     view.initGraphCtrl(this.uri.path, undefined);
     view.loadContent();
-    this._circleViewer.push(view);
+    this._circleEditor.push(view);
 
     panel.onDidDispose(() => {
       // TODO make faster
-      this._circleViewer.forEach((view, index) => {
+      this._circleEditor.forEach((editor, index) => {
         if (view.owner(panel)) {
           view.disposeGraphCtrl();
-          this._circleViewer.splice(index, 1);
+          this._circleEditor.splice(index, 1);
         }
       });
     });
@@ -93,21 +232,18 @@ export class CircleViewerDocument implements vscode.CustomDocument {
   }
 };
 
-/**
- * @brief Circle model viewer readonly Provider
- */
-/* istanbul ignore next */
-export class CircleViewerProvider implements
-    vscode.CustomReadonlyEditorProvider<CircleViewerDocument> {
-  public static readonly viewType = 'one.viewer.circle';
+
+export class CircleEditorProvider implements
+    vscode.CustomEditorProvider<CircleEditorDocument> {
+  public static readonly viewType = 'one.editor.circle'; //priority 높여야 함
 
   private _context: vscode.ExtensionContext;
 
   public static register(context: vscode.ExtensionContext): void {
-    const provider = new CircleViewerProvider(context);
+    const provider = new CircleEditorProvider(context);
 
     const registrations = [
-      vscode.window.registerCustomEditorProvider(CircleViewerProvider.viewType, provider, {
+      vscode.window.registerCustomEditorProvider(CircleEditorProvider.viewType, provider, {
         webviewOptions: {
           retainContextWhenHidden: true,
         },
@@ -122,23 +258,69 @@ export class CircleViewerProvider implements
     this._context = context;
   }
 
-  // CustomReadonlyEditorProvider implements
-  async openCustomDocument(
-      uri: vscode.Uri, openContext: {backupId?: string},
-      _token: vscode.CancellationToken): Promise<CircleViewerDocument> {
-    const document: CircleViewerDocument = await CircleViewerDocument.create(uri);
-    // NOTE as a readonly viewer, there is not much to do
+  // private readonly _onDidChangeCustomDocument = new vscode.EventEmitter<vscode.CustomDocumentEditEvent<CircleEditorDocument>>();
+	// public readonly onDidChangeCustomDocument = this._onDidChangeCustomDocument.event;
 
-    // TODO handle dispose
-    // TODO handle file change events
-    // TODO handle backup
-
-    return document;
+  onDidChangeCustomDocument: vscode.Event<vscode.CustomDocumentEditEvent<CircleEditorDocument>> | vscode.Event<vscode.CustomDocumentContentChangeEvent<CircleEditorDocument>>;
+  saveCustomDocument(document: CircleEditorDocument, cancellation: vscode.CancellationToken): Thenable<void> {
+    throw new Error('Method not implemented.');
   }
+  saveCustomDocumentAs(document: CircleEditorDocument, destination: vscode.Uri, cancellation: vscode.CancellationToken): Thenable<void> {
+    throw new Error('Method not implemented.');
+  }
+  revertCustomDocument(document: CircleEditorDocument, cancellation: vscode.CancellationToken): Thenable<void> {
+    throw new Error('Method not implemented.');
+  }
+  backupCustomDocument(document: CircleEditorDocument, context: vscode.CustomDocumentBackupContext, cancellation: vscode.CancellationToken): Thenable<vscode.CustomDocumentBackup> {
+    throw new Error('Method not implemented.');
+  }
+
+
+	async openCustomDocument(
+		uri: vscode.Uri,
+		openContext: { backupId?: string },
+		_token: vscode.CancellationToken
+	): Promise<CircleEditorDocument> {
+		const document: CircleEditorDocument = await CircleEditorDocument.create(uri, openContext.backupId, {
+			getFileData: async () => {
+				const webviewsForDocument = Array.from(this.webviews.get(document.uri));
+				if (!webviewsForDocument.length) {
+					throw new Error('Could not find webview to save for');
+				}
+				const panel = webviewsForDocument[0];
+				const response = await this.postMessageWithResponse<number[]>(panel, 'getFileData', {});
+				return new Uint8Array(response);
+			}
+		});
+
+		const listeners: vscode.Disposable[] = [];
+
+		listeners.push(document.onDidChange(e => {
+			// Tell VS Code that the document has been edited by the use.
+			this._onDidChangeCustomDocument.fire({
+				document,
+				...e,
+			});
+		}));
+
+		listeners.push(document.onDidChangeContent(e => {
+			// Update all webviews when the document changes
+			for (const webviewPanel of this.webviews.get(document.uri)) {
+				this.postMessage(webviewPanel, 'update', {
+					edits: e.edits,
+					content: e.content,
+				});
+			}
+		}));
+
+		document.onDidDispose(() => disposeAll(listeners));
+
+		return document;
+	}
 
   // CustomReadonlyEditorProvider implements
   async resolveCustomEditor(
-      document: CircleViewerDocument, webviewPanel: vscode.WebviewPanel,
+      document: CircleEditorDocument, webviewPanel: vscode.WebviewPanel,
       _token: vscode.CancellationToken): Promise<void> {
     document.openView(webviewPanel, this._context.extensionUri);
   }
